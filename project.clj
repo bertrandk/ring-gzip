b4aaec0764234696bacb6ad0dc12bbb8434f4dff
bertrandk <bertrandk@outlook.com>
2016-12-28 18:11:31 -0500

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bk/ring-gzip "0.1.1"
+(defproject bk/ring-gzip "0.2.0"
   :description "Middleware that compresses repsonses with gzip for supported
                user-agents."
   :url "https://github.com/bertrandk/ring-gzip"
